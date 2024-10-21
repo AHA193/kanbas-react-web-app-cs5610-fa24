865563eee13f420a11b6856fc88b42ff71510ba8
Aziz <azizhoumani@gmail.com>
2024-10-21 18:01:43 +0200

    a1 lab

---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,116 @@
+export default function Lab1() {
+    return (<div id="wd-lab1">
+        <h2>Lab 1</h2>
+        <h3>HTML Examples</h3>
+        <div id="wd-h-tag">
+            <h4>Heading Tags</h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eius reiciendis voluptatum molestias
+             sit blanditiis doloribus praesentium qui consequuntur numquam rem cumque saepe debitis laboriosam 
+             placeat, ipsam laborum ipsum expedita culpa ea et! Voluptatum asperiores nostrum minima, ab neque 
+             omnis fugiat assumenda unde aperiam ex hic commodi nihil quidem beatae. Eaque iure deserunt sit
+              doloribus vel laboriosam officiis explicabo? Voluptatem illum, itaque accusantium nostrum saepe 
+              quis obcaecati reiciendis sapiente necessitatibus, earum tempore eius officia. Sit at facere ut 
+              aperiam iste adipisci, obcaecati soluta omnis dolores rem id commodi molestiae odit autem 
+              voluptatem similique nesciunt porro perferendis, quis quas. Consequuntur laudantium temporibus 
+              fugit esse iusto architecto voluptatibus quia est molestiae! Provident tenetur odio voluptate 
+              consectetur nemo sapiente error culpa, commodi perferendis alias, necessitatibus dignissimos 
+              accusamus quam eius aperiam ipsa nesciunt omnis expedita repudiandae deleniti eaque suscipit.
+              <h4>Paragraphs</h4> 
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellat veniam temporibus 
+                culpa porro ex quibusdam reprehenderit accusamus, delectus nostrum expedita blanditiis 
+                natus minus hic esse commodi. Deleniti, atque debitis. Cumque est quaerat incidunt a, 
+                quas debitis. Excepturi saepe nostrum magnam distinctio, est enim ratione dolorem debitis 
+                error at iure!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellat veniam temporibus 
+                culpa porro ex quibusdam reprehenderit accusamus, delectus nostrum expedita blanditiis 
+                natus minus hic esse commodi. Deleniti, atque debitis. Cumque est quaerat incidunt a, 
+                quas debitis. Excepturi saepe nostrum magnam distinctio, est enim ratione dolorem debitis 
+                error at iure!
+              </p>
+              <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                <ol>
+                    <li>Exemple 1</li>
+                    <li>Exemple 2</li>
+                    <li>Exemple 3</li>
+                    <li>Exemple 4</li>
+                    <li>Exemple 5</li>
+                    <li>Exemple 6</li>
+                    <li>Exemple 7</li>
+                    <li>Exemple 8</li>
+                </ol>
+              </div>
+
+
+              <div id="wd-tables">
+            <h4>Table Tag</h4>
+        
+<table border ={1} width="100%">
+<thead>  
+    <tr>  
+        <th>Quiz</th>  
+        <th>Topics</th>  
+        <th>Date</th>  
+	<th>Grade</th>  
+    </tr>  
+</thead>  
+<tbody>  
+    <tr>  
+        <td>Q1</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+    <tr>  
+        <td>Q2</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+    <tr>  
+        <td>Q3</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+    <tr>  
+        <td>Q4</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+    <tr>  
+        <td>Q1</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+    <tr>  
+        <td>Q1</td>  
+        <td>HTML</td>  
+        <td>2/3/21</td>  
+    	<td>85</td>  
+    </tr>  
+
+    </tbody>  
+    <tfoot>
+    <tr> 
+        <td colSpan={3}>Average</td>  
+        <td>90</td>  
+    </tr>  
+    </tfoot> 
+    </table>  
+
+
+        </div>
+
+
+        </div>
+        </div>
+    );
+}
